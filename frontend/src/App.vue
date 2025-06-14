7be16ae7023f9b30ea6ba7ba761d51c373ee10b8
Mrinal Sinha <mail@themrinalsinha.com>
2025-06-15 01:50:15 +0530

    simple title

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-
+import DefaultLayout from './layouts/DefaultLayout.vue';
 </script>
 
 <template>
-  <h1>Hello InHook! :)</h1>
+  <DefaultLayout>
+    <header class="bg-white shadow-sm">
+      <nav class="mx-auto flex items-center p-4">
+        <h1 class="text-xl font-bold text-gray-900">inHook</h1>
+      </nav>
+    </header>
+  </DefaultLayout>
 </template>
---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
