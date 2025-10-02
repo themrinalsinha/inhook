ca3200c673a1f6eeb7cd936587c3ee2a819d440f
Mrinal Sinha <mail@themrinalsinha.com>
2025-10-02 10:58:54 +0530

    dynamic title for webpage

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react'
 import { Container } from '../Container'
 
 export const Dashboard = () => {
+
+  useEffect(() => {
+    document.title = 'InHook - Dashboard'
+  }, [])
+
   return (
     <Container>
       <div>Dashboard</div>
--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,15 @@
+import { useEffect } from 'react'
 import { NavBar } from './NavBar'
 import { Hero } from './Hero'
 import { Footer } from './Footer'
 import { Container } from '../Container'
 
 export const LandingPage = () => {
+
+  useEffect(() => {
+    document.title = 'InHook - Webhook Inspector & Debugger'
+  }, [])
+
   return (
     <Container className="h-screen bg-linear-to-b from-blue-50 to-white">
       <div className="max-w-6xl mx-auto relative bg-linear-to-b to-white from-white rounded-3xl h-full">
