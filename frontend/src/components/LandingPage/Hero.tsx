dc0a4fb3ceece4cac08a1ebbc436561b0f42357a
Mrinal Sinha <mail@themrinalsinha.com>
2025-10-04 18:59:22 +0530

    some thing added - just for fun

--- frontend/src/components/LandingPage/Hero.tsx
+++ frontend/src/components/LandingPage/Hero.tsx
@@ -2,22 +2,30 @@ import { Link } from 'react-router'
 import { cn } from '../../libs/util';
 import { ChevronRight } from "lucide-react";
 import heroImage from '../../assets/splash.png';
+import { Circle } from "lucide-react";
 
 export const Hero = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
       <h1
         className={cn(
+          "relative",
           "py-5 md:text-6xl text-4xl font-semibold tracking-tight max-w-2xl text-center",
           "bg-clip-text text-transparent bg-gradient-to-b from-neutral-700 to-neutral-500"
         )}
       >
+        <Circle
+          className="absolute -top-2 right-0 size-10 animate-pulse stroke-blue-thm/20"
+          strokeWidth={3}
+        />
         Self-hosted webhook inspector and debugger
       </h1>
-      <p className={cn(
-        "md:text-[25px] text-lg tracking-normal font-light max-w-2xl text-center",
-        "text-neutral-400 mt-2"
-      )}>
+      <p
+        className={cn(
+          "md:text-[25px] text-lg tracking-normal font-light max-w-2xl text-center",
+          "text-neutral-400 mt-2"
+        )}
+      >
         A single binary tool to capture, inspect and debug incoming webhooks in
         real time.
       </p>
@@ -37,7 +45,11 @@ export const Hero = () => {
           <ChevronRight className="w-5 h-5 ml-2" />
         </div>
       </Link>
-      <div className="mt-10 max-w-5xl mx-auto p-2">
+      <div className="relative mt-10 max-w-5xl mx-auto p-2">
+        <Circle
+          className="absolute -top-35 -left-15 size-20 animate-pulse stroke-blue-thm/20"
+          strokeWidth={2}
+        />
         <img
           src={heroImage}
           alt="InHook - Webhook Inspector & Debugger"
