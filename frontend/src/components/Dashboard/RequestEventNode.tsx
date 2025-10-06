290a295d439ebc2866f6899507b229b20af4e339
Mrinal Sinha <mail@themrinalsinha.com>
2025-10-07 01:34:57 +0530

    skip loading color dynamically to fix the issue

--- frontend/src/components/Dashboard/RequestEventNode.tsx
+++ frontend/src/components/Dashboard/RequestEventNode.tsx
@@ -3,43 +3,53 @@ import { ChevronRight, Plus, Trash, Circle, Pencil } from "lucide-react";
 
 type RequestMethod = "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
 
-const requestMethods: Record<RequestMethod, { color: string; icon: React.ElementType }> = {
+const requestMethods: Record<RequestMethod, {
+  bgColor: string;
+  textColor: string;
+  icon: React.ElementType
+}> = {
   GET: {
-    color: "green",
+    bgColor: "bg-green-50",
+    textColor: "text-green-500",
     icon: Circle,
   },
   POST: {
-    color: "orange",
+    bgColor: "bg-orange-50",
+    textColor: "text-orange-500",
     icon: Plus,
   },
   PATCH: {
-    color: "yellow",
+    bgColor: "bg-yellow-50",
+    textColor: "text-yellow-500",
     icon: Pencil,
   },
   DELETE: {
-    color: "red",
+    bgColor: "bg-red-50",
+    textColor: "text-red-500",
     icon: Trash,
   },
   PUT: {
-    color: "blue",
+    bgColor: "bg-blue-50",
+    textColor: "text-blue-500",
     icon: Pencil,
   },
 };
 
 export const RequestEventNode = ({ method }: { method: RequestMethod }) => {
-  const { color, icon: Icon } = requestMethods[method];
+  const { bgColor, textColor, icon: Icon } = requestMethods[method];
 
   return (
     <div>
       <div
         className={cn(
           "flex flex-col justify-between h-12 px-1 border-1 border-neutral-200 rounded-sm",
-          `bg-${color}-50 hover:border-neutral-700 hover:border-1 hover:cursor-pointer`,
+          bgColor,
+          "hover:border-neutral-700 hover:border-1 hover:cursor-pointer",
           "transition-all duration-300"
         )}
       >
         <div className="flex items-baseline justify-between">
-          <div className={`flex items-center gap-1 text-sm font-bold text-${color}-500`}>
+          <div className={cn("flex items-center gap-1 text-sm font-bold", textColor)}>
             <Icon className="size-3" />
             {method}
           </div>
