d196706e6a061eaafe86ed24bdb51feeba3465bf
Mrinal Sinha <mail@themrinalsinha.com>
2025-10-06 21:51:39 +0530

    making custom scroll for events section

--- frontend/src/components/Dashboard/SidePanel.tsx
+++ frontend/src/components/Dashboard/SidePanel.tsx
@@ -7,8 +7,15 @@ export const SidePanel = ({ className }: { className?: string }) => {
         <p className="text-md font-semibold text-neutral-700">Request Info</p>
         <Badge variant="outline">Events</Badge>
       </div>
-      <div className="outline-1 outline-neutral-100 overscroll-contain rounded-lg">
-        <div className="bg-neutral-50/50 rounded-lg mt-2 p-2 h-[calc(100vh-250px)] border-1 border-neutral-100 overflow-y-auto">
+      <div className="bg-neutral-50/50 outline-1 outline-neutral-200 overscroll-contain rounded-lg mt-3 py-1">
+        <div
+          className=" rounded-lg p-2 h-[calc(100vh-262px)] overflow-y-auto"
+          style={{
+            scrollbarWidth: "thin",
+            scrollbarColor:
+              "var(--color-neutral-200) var(--color-neutral-50/50)", // thumb color, track color
+          }}
+        >
           <div className="border-1 border-neutral-200 rounded-sm mt-2 h-10"></div>
           <div className="border-1 border-neutral-200 rounded-sm mt-2 h-10"></div>
           <div className="border-1 border-neutral-200 rounded-sm mt-2 h-10"></div>
