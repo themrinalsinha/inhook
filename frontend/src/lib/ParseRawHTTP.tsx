4dc6cd1c96a33818b28278f8720494ae59af86d6
Mrinal Sinha <mail@themrinalsinha.com>
2025-10-19 17:34:40 +0530

    parse multipart/form-data text values

--- frontend/src/lib/ParseRawHTTP.tsx
+++ frontend/src/lib/ParseRawHTTP.tsx
@@ -3,24 +3,58 @@
 import type { ReactNode } from "react";
 import { useMemo } from "react";
 import parser from "http-message-parser";
+import { TableViewer } from "@/components/Utils/Viewers";
+
+const _parseHeaders = (headers: Record<string, any>) : Record<string, any> => {
+  const headersObj: Record<string, any> = {};
+  for (const [key, value] of Object.entries(headers)) {
+    const _key = key.toLowerCase();
+    if (_key === "content-disposition" || _key === "content-type") {
+      headersObj[_key] = {};
+      const [mainType, ...params] = value.split(";");
+      headersObj[_key].type = mainType.trim();
+      params.forEach((param) => {
+        const [pKey, pVal] = param.split("=").map((x) => x && x.trim());
+        if (pKey && pVal !== undefined) {
+          // Remove quotes if any
+          headersObj[_key][pKey] = pVal.replace(/^"|"$/g, "");
+        }
+      });
+    } else {
+      headersObj[_key] = value;
+    }
+  }
+  return headersObj;
+};
+
+const _getDecodedBody = (body: Uint8Array) => {
+  const text = new TextDecoder().decode(body);
+  return text.trim();
+}
 
 const _processMultipartData = (rawData: string) => {
   const binary = atob(rawData);
   const parsed = parser(binary);
 
-  const formData = {}
-  const multipartData = parsed.multipart
+  const formData = {};
+  const multipartData = parsed.multipart;
 
-  // TODO: parse and process multipart data
-
-  return null
-}
+  for (const data of multipartData) {
+    const headers = _parseHeaders(data.headers);
+    if (!headers["content-type"]) {
+      formData[headers["content-disposition"]?.name] = _getDecodedBody(
+        data.body
+      );
+    }
+  }
+  return formData;
+};
 
 export const GetParsedMultipartData = (
   { rawData }: { rawData: string }
 ): ReactNode => {
-  _processMultipartData(rawData)
-  return <></>
+  const formData = _processMultipartData(rawData);
+  return <TableViewer data={formData} />
 };
 
 export function ParseRawHTTP({ rawData }: { rawData: string }) {
