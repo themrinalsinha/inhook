941d28a78bc0003b12d1e19ff6a09d4343ce6450
aak-301 <aakashshivanshu5@gmail.com>
2025-10-24 11:02:12 +0530

    Added the buffer package in the vite config used by http-message-parser

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -32,4 +32,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    include: ['buffer']
+  }
 })
